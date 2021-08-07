15d9fcfd7d5da26577660b13c2a50f2dd4302579
Logan Dam <logan.dam@entelect.co.za>
2021-08-07 15:30:57 +0200

    fix build

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ldam.co.za.fnapp.Services;
 using ldam.co.za.lib.Services;
 
 namespace ldam.co.za.backend
