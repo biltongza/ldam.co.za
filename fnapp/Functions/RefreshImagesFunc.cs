663ee63c5cfc2817e8d869a3656a1218c6c63b46
Logan Dam <ldam@silica.net>
2021-08-19 17:19:22 +0000

    Skip running function if we are past due (simplifies debugging)

--- fnapp/Functions/RefreshImagesFunc.cs
+++ fnapp/Functions/RefreshImagesFunc.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using ldam.co.za.fnapp.Services;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace ldam.co.za.fnapp.Functions
 {
@@ -16,6 +17,12 @@ namespace ldam.co.za.fnapp.Functions
         [Function(nameof(RefreshImagesFunc))]
         public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo timerInfo, FunctionContext functionContext)
         {
+            var logger = functionContext.GetLogger<RefreshImagesFunc>();
+            if(timerInfo.IsPastDue)
+            {
+                logger.LogInformation("Timer is late, skipping");
+                return;
+            }
             await syncService.SyncImages();
         }
     }
