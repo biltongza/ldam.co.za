9e57058dbc37fa395de42e39f675de1f766b1c69
Logan Dam <ldam@silica.net>
2021-08-20 20:55:13 +0000

    add app insights

--- client/Program.cs
+++ client/Program.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Net.Http;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using BlazorApplicationInsights;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace ldam.co.za.client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazorApplicationInsights();
 
             await builder.Build().RunAsync();
         }
