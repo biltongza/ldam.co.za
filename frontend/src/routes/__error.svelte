f1a69859f88c454d9e0ff14ade3389bd5e61d7e8
Logan Dam <logan.dam@entelect.co.za>
2021-10-19 09:41:47 +0000

    app insights tracking

--- frontend/src/routes/__error.svelte
+++ frontend/src/routes/__error.svelte
@@ -1,1 +1,28 @@
+<script context="module" lang="ts">
+	import type { IApplicationInsights } from '@microsoft/applicationinsights-web';
+	import type { ErrorLoad } from '@sveltejs/kit';
+
+	const appInsights: IApplicationInsights = (<any>window).appInsights;
+	const load: ErrorLoad = function ({ error, status, page }) {
+		appInsights.trackException({
+			error: error,
+            properties: {
+                status,
+                ...page,
+            },
+		});
+		return {
+            props: {
+                status,
+            }
+        };
+	};
+
+	export { load };
+</script>
+<script lang="ts">
+    export let status: number;
+</script>
+
+<h3>{status}</h3>
 <p>Something went wrong :(</p>
