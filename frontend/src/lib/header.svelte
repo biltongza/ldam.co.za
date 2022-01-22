cc3312905fba95e96fad7bb435bab1201e2c17fc
Logan Dam <dam.loganza@gmail.com>
2022-01-22 17:19:15 +0200

    implement mobile nav menu

--- frontend/src/lib/header.svelte
+++ frontend/src/lib/header.svelte
@@ -1,57 +1,75 @@
-<script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import SocialIcon from './header/social-icon.svelte';
 	import RouterLink from './router-link.svelte';
+	let smol = false;
+	let expanded = false;
+	onMount(() => {
+		const matchListener = (event: MediaQueryListEvent) => {
+			smol = event.matches;
+		};
+		const mql = window.matchMedia('(max-width: 480px)');
+		smol = mql.matches;
+		mql.addEventListener('change', matchListener);
+		return () => {
+			mql.removeEventListener('change', matchListener);
+		};
+	});
+
+	const routes = [
+		{ label: 'About', path: '/about' },
+		{ label: 'Blog', path: '/blog' }
+	];
+
+	const socials = [
+		{ text: 'GitHub', label: 'github', href: 'https://github.com/biltongza', icon: 'github' },
+		{
+			text: 'LinkedIn',
+			label: 'linkedin',
+			href: 'https://www.linkedin.com/in/logan-dam/',
+			icon: 'linkedin'
+		},
+		{
+			text: 'Instagram',
+			label: 'instagram',
+			href: 'https://www.instagram.com/thebiltong/',
+			icon: 'instagram'
+		},
+		{ text: 'Twitter', label: 'twitter', href: 'https://twitter.com/TheBiltong', icon: 'twitter' }
+	];
 </script>
 
 <header class="header">
-	<div class="brand"><a href="/"><h1>Logan Dam</h1></a></div>
-	<div class="links">
-		<nav>
-			<RouterLink path="/about">About</RouterLink>
-			<RouterLink path="/blog">Blog</RouterLink>
+	<div class="nav-container">
+		<a class="brand" href="/"><h1>Logan Dam</h1></a>
+		<nav class={smol && expanded ? 'expanded' : ''}>
+			<ul>
+				{#if smol}
+					<li><RouterLink path="/">Home</RouterLink></li>
+				{/if}
+				{#each routes as { label, path }}
+					<li><RouterLink {path}>{label}</RouterLink></li>
+				{/each}
+				{#each socials as { text, label, href, icon }}
+					<li>
+						<SocialIcon {text} {label} {href} {icon} showText={smol && expanded} />
+					</li>
+				{/each}
+			</ul>
 		</nav>
-		<div class="socials">
-			<sl-icon-button
-				name="github"
-				target="_blank"
-				label="github"
-				href="https://github.com/biltongza"
-			/>
-			<sl-icon-button
-				name="linkedin"
-				target="_blank"
-				label="linkedin"
-				href="https://www.linkedin.com/in/logan-dam/"
-			/>
-			<sl-icon-button
-				name="instagram"
-				target="_blank"
-				label="instagram"
-				href="https://www.instagram.com/thebiltong/"
-			/>
-			<sl-icon-button
-				name="twitter"
-				target="_blank"
-				label="twitter"
-				href="https://twitter.com/TheBiltong"
-			/>
-		</div>
 	</div>
+	{#if smol}
+		<sl-icon-button
+			class={`menu-button ${expanded ? 'expanded' : ''}`}
+			name="chevron-double-down"
+			on:click={() => (expanded = !expanded)}
+		/>
+	{/if}
 </header>
 
-<style>
-	.header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		height: 60px;
-		background-color: var(--sl-color-neutral-100);
-		margin-bottom: var(--sl-spacing-small);
-	}
-
-	.links {
-		display: flex;
-		justify-content: flex-end;
-		align-items: center;
+<style lang="scss">
+	:root {
+		--header-height: 60px;
 	}
 
 	h1 {
@@ -62,17 +80,85 @@
 	.header {
 		padding-left: var(--sl-spacing-small);
 		padding-right: var(--sl-spacing-small);
-	}
+		background-color: var(--sl-color-neutral-100);
+		margin-bottom: var(--sl-spacing-small);
+		display: flex;
+		flex-direction: row;
+		align-items: start;
 
-	@media only screen and (min-width: 1224px) {
-		.header {
+		@media only screen and (min-width: 1224px) {
+			flex-direction: column;
 			padding-left: var(--sl-spacing-2x-large);
 			padding-right: var(--sl-spacing-2x-large);
 		}
 	}
-	nav {
+
+	.nav-container {
+		flex: 1 0 auto;
 		display: flex;
+		flex-direction: row;
 		justify-content: space-between;
-		gap: 1em;
+		align-items: center;
+		@media screen and (max-width: 480px) {
+			flex-direction: column;
+			align-items: flex-start;
+			font-size: var(--sl-font-size-large);
+		}
+	}
+
+	.brand,
+	.menu-button {
+		margin-top: var(--sl-spacing-small);
+		margin-bottom: var(--sl-spacing-small);
+		font-size: var(--sl-font-size-x-large);
+	}
+
+	.menu-button {
+		flex: 0 0 auto;
+		margin-top: var(--sl-spacing-medium);
+	}
+
+	nav {
+		@media screen and (max-width: 480px) {
+			margin-bottom: var(--sl-spacing-medium);
+			display: none;
+			background-color: var(--sl-color-neutral-150);
+			&.expanded {
+				display: unset;
+				width: 100%;
+				ul {
+					flex-direction: column;
+					align-items: flex-start;
+				}
+			}
+		}
+		ul {
+			list-style: none;
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			gap: var(--sl-spacing-medium);
+			padding-inline-start: 0;
+			margin: 0;
+			li {
+				min-height: 30px;
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+			}
+		}
+	}
+	.menu-button {
+		display: none;
+		@media only screen and (max-width: 480px) {
+			display: block;
+			transform: rotate(0deg);
+			transition: all 0.25s ease-in;
+
+			&.expanded {
+				transform: rotate(180deg);
+				transition: all 0.25s ease-out;
+			}
+		}
 	}
 </style>
---
+++ frontend/src/lib/header/social-icon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let showText = false;
+    export let text: string;
+    export let label: string;
+    export let icon: string;
+    export let href: string;
+</script>
+
+<sl-button
+	variant="default"
+	size="medium"
+	href={href}
+	target="_blank"
+	label={label}
+>
+	<sl-icon slot="prefix" name={icon} />
+	{showText ? text : ''}
+</sl-button>
+
+
+<style lang="scss">
+    sl-button::part(base) {
+		border-radius: unset;
+		background-color: unset;
+		color: unset;
+		border: none;
+
+		padding: unset;
+		&:hover {
+			color: var(--sl-color-primary-600);
+		}
+	}
+	sl-button::part(label) {
+		font-family: 'Recursive', monospace;
+		line-height: unset;
+	}
+</style>
