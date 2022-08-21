e2de7ef2e592b3508eca5db7352341982bdfaf6e
Logan Dam <dam.loganza@gmail.com>
2022-08-21 16:21:54 +0200

    Add title & social metadata

--- frontend/src/routes/collections/index.svelte
+++ frontend/src/routes/collections/index.svelte
@@ -1,12 +1,22 @@
 <script context="module" lang="ts">
-	import AlbumPreview from '$lib/album/album-preview.svelte';
-	import type { Album,Manifest } from '$lib/types';
-	import type { Load } from '@sveltejs/kit';
+    import AlbumPreview from '$lib/album/album-preview.svelte';
+    import { metaStore,titleStore } from '$lib/stores';
+    import type { Album,Manifest } from '$lib/types';
+    import type { Load } from '@sveltejs/kit';
 
 	let collections: Album[];
 	export const load: Load = function ({ stuff }) {
 		let manifest: Manifest = stuff.manifest;
 		collections = manifest.albums.filter((x) => !x.isPortfolio);
+
+        metaStore.set({
+			'twitter:card': 'summary',
+			'og:type': 'article',
+			'og:title': 'Collections',
+			'og:description': 'Photography by Logan Dam',
+		});
+
+        titleStore.set('Collections');
 
 		return {};
 	};
