18b76ff247090aea51ecbd98c554d0abb69060e0
Logan Dam <dam.loganza@gmail.com>
2023-04-30 21:44:48 +0200

    fix ordering of images

--- frontend-ng/src/app/components/album-preview/album-preview.component.ts
+++ frontend-ng/src/app/components/album-preview/album-preview.component.ts
@@ -1,28 +1,34 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { Album, ImageMetadata } from 'src/app/types';
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { Album, ImageMetadata } from "src/app/types";
 
 @Component({
-  selector: 'app-album-preview',
-  templateUrl: './album-preview.component.html',
-  styleUrls: ['./album-preview.component.scss']
+  selector: "app-album-preview",
+  templateUrl: "./album-preview.component.html",
+  styleUrls: ["./album-preview.component.scss"],
 })
 export class AlbumPreviewComponent implements OnChanges {
-  
   @Input() album?: Album;
-	@Input() numberOfImages: number = Number.MAX_SAFE_INTEGER;
+  @Input() numberOfImages: number = Number.MAX_SAFE_INTEGER;
 
   images?: ImageMetadata[];
 
   ngOnChanges(changes: SimpleChanges): void {
-    if(changes['album'] && this.album) {
-      this.images = Object.entries(this.album.images).map(([_, x]) => x);
+    if ((changes["album"] || changes["numberOfImages"]) && this.album) {
+      const entries = Object.entries(this.album.images).map(([, meta]) => meta);
+      this.images = entries
+        .sort((meta1, meta2) => {
+          const a = new Date(meta1.captureDate).valueOf();
+          const b = new Date(meta2.captureDate).valueOf();
+
+          return Number(b > a) - Number(b < a);
+        })
+        .slice(0, this.numberOfImages);
     }
   }
 
   getClassList(image: ImageMetadata): string {
-    const parts = image.aspectRatio.split(':').map((part) => +part);
+    const parts = image.aspectRatio.split(":").map((part) => +part);
     const isLandscape = parts[0] > parts[1];
-    return `thumbnail ${isLandscape ? 'span-col' : ''}`;
+    return `thumbnail ${isLandscape ? "span-col" : ""}`;
   }
-  
 }
