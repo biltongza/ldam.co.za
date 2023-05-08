a1fd3f104e74ef68ab5b50ca449bff0f0af12dff
Logan Dam <dam.loganza@gmail.com>
2023-05-08 19:45:41 +0200

    standalone component migration step 3 (step 2 was not necessary)

--- frontend-ng/src/app/app.component.ts
+++ frontend-ng/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'frontend-ng';
--- frontend-ng/src/main.ts
+++ frontend-ng/src/main.ts
@@ -1,9 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 
 import { registerIconLibrary } from '@shoelace-style/shoelace';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.4.0/dist/');
 
@@ -17,5 +22,10 @@ registerIconLibrary('ionicons', {
   }
 });
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
