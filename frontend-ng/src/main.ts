63e1e2d9f43e28ed85d15d91ab6d60b8bd4f8734
Logan Dam <dam.loganza@gmail.com>
2023-05-19 16:55:09 +0200

    use shoelace autoloader because angular doesn't know how to tree-shake it

--- frontend-ng/src/main.ts
+++ frontend-ng/src/main.ts
@@ -1,28 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { registerIconLibrary } from '@shoelace-style/shoelace';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
 setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.4.0/dist/');
-
-registerIconLibrary('ionicons', {
-	resolver: (name) => `https://cdn.jsdelivr.net/npm/ionicons@5.1.2/dist/ionicons/svg/${name}.svg`,
-	mutator: (svg) => {
-		svg.setAttribute('fill', 'currentColor');
-		svg.setAttribute('stroke', 'currentColor');
-		Array.from(svg.querySelectorAll('.ionicon-fill-none')).map((el) =>
-			el.setAttribute('fill', 'none')
-		);
-		Array.from(svg.querySelectorAll('.ionicon-stroke-width')).map((el) =>
-			el.setAttribute('stroke-width', '32px')
-		);
-	}
-});
 
 bootstrapApplication(AppComponent, {
 	providers: [
