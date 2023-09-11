0e1d075cafad036296edf8a7f832cbf69afacbaf
Logan Dam <dam.loganza@gmail.com>
2023-09-11 12:27:52 +0200

    add logging to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,18 @@ import { exec } from 'child_process';
 import { promisify } from 'util';
 
 // Get current tag/commit and last commit date from git
-const pexec = promisify(exec)
-let [version, lastmod] = (
+const pexec = promisify(exec);
+const promises = (
   await Promise.allSettled([
     pexec('git describe --tags || git rev-parse --short HEAD'),
     pexec('git log -1 --format=%cd --date=format:"%Y-%m-%d %H:%M"'),
   ])
-).map(v => JSON.stringify(v.value?.stdout.trim()))
+);
+console.log('VITE CONFIG: promises', promises);
+
+const [version, lastmod] = promises.map(v => JSON.stringify(v.value?.stdout.trim()));
+console.log(`VITE CONFIG: commit hash`, version);
+console.log(`VITE CONFIG: commit time`, lastmod);
 
 /** @type {import('vite').UserConfig} */
 const config = {
