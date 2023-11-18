3692df6fe4380dd7adae9c06cd5227d5f9b3b653
Logan Dam <dam.loganza@gmail.com>
2023-11-18 22:19:11 +0100

    switch to await

--- frontend/generate-last-modified-metadata.js
+++ frontend/generate-last-modified-metadata.js
@@ -2,16 +2,9 @@ import fs from 'fs';
 import { glob } from 'glob';
 import path from 'path';
 const routesPath = path.join('src', 'routes');
-glob(`${routesPath}/**/*.svelte`).then((matches) => {
-	const lastModified = matches.map((match) => ({
-		path: match,
-		lastModified: fs.statSync(match).mtimeMs
-	}));
-	const lastModifiedJson = `export const metadata = ${JSON.stringify(lastModified)}`;
-	fs.writeFileSync(path.join('src', 'lib', '.metadata.js'), lastModifiedJson);
-	console.log('wrote thing!');
-}, (err) => {
-	if (err) {
-		throw err;
-	}
-});
+const lastModified = (await glob(`${routesPath}/**/*.svelte`)).map((match) => ({
+	path: match,
+	lastModified: fs.statSync(match).mtimeMs
+}));
+const lastModifiedJson = `export const metadata = ${JSON.stringify(lastModified)}`;
+fs.writeFileSync(path.join('src', 'lib', '.metadata.js'), lastModifiedJson);
