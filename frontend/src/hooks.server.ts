ca5ea0ee15916118f805d2ce346e242aac3aefab
Logan Dam <dam.loganza@gmail.com>
2023-11-19 11:17:52 +0100

    add request tracing

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,10 +1,46 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
 export function handleError({ error, event }) {
-  if (event?.platform?.context?.log?.error && typeof event.platform.context.log.error === 'function') {
-    const {method, url, referrer, headers} = event.request;
-    event.platform.context.log.error(error, {method, url, referrer, headers: Object.fromEntries(headers.entries())});
+  try {
+    if (
+      event?.platform?.context?.log?.error &&
+      typeof event.platform.context.log.error === 'function'
+    ) {
+      const details = getSimpleRequestDetails(event);
+      event.platform.context.log.error({ error, details });
+    }
+  } catch (err) {
+    console.error(err);
   }
 
   return {
     message: 'Something went wrong :('
   };
+}
+
+export async function handle({ event, resolve }) {
+  const response = await resolve(event);
+  try {
+    if (
+      event?.platform?.context?.log?.info &&
+      typeof event.platform.context.log.info === 'function'
+    ) {
+      const simpleRequest = getSimpleRequestDetails(event);
+      const simpleResponse = getSimpleResponseDetails(response);
+      event.platform.context.log.info({ request: simpleRequest, response: simpleResponse });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+  return response;
+}
+
+function getSimpleRequestDetails(event: RequestEvent) {
+  const { method, url, referrer, headers } = event.request;
+  return { method, url, referrer, headers: Object.fromEntries(headers.entries()) };
+}
+
+function getSimpleResponseDetails(response: Response) {
+  const { status, headers } = response;
+  return { status, headers: Object.fromEntries(headers.entries()) };
 }
