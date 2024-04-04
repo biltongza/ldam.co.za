083260594203d91b006fc01ba90e00b4081daf15
Logan Dam <dam.loganza@gmail.com>
2024-04-05 00:55:44 +0200

    add calendar icon to date

--- frontend/src/routes/blog/+page.svelte
+++ frontend/src/routes/blog/+page.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
-	import { base } from '$app/paths';
-	import { DateFormat } from '$lib/__consts';
-	import dayjs from 'dayjs';
-	import type { PageData } from './$types';
-	export let data: PageData;
+  import { base } from '$app/paths';
+  import { DateFormat } from '$lib/__consts';
+  import dayjs from 'dayjs';
+  import type { PageData } from './$types';
+  export let data: PageData;
 
-	export let {posts} = data;
+  export let { posts } = data;
 </script>
 
 <div>
-	<p class="info">{posts.length} posts.</p>
-	{#each posts as post}
-		<div class="post">
-			<a href={`${base}/blog/${post.slug}`}>
-				<h2 class="title">{post.title}</h2>
-			</a>
-			<p class="date">{dayjs(post.date).format(DateFormat)}</p>
-			<p>
-				{#each post.tags as tag}
-					<sl-badge variant="primary" pill>{tag}</sl-badge>
-				{/each}
-			</p>
-			<p>{post.excerpt}</p>
-		</div>
-	{/each}
+  <p class="info">{posts.length} posts.</p>
+  {#each posts as post}
+    <div class="post">
+      <a href={`${base}/blog/${post.slug}`}>
+        <h2 class="title">{post.title}</h2>
+      </a>
+      <p class="date"><sl-icon name="calendar-event" />{dayjs(post.date).format(DateFormat)}</p>
+      <p>
+        {#each post.tags as tag}
+          <sl-badge variant="primary" pill>{tag}</sl-badge>
+        {/each}
+      </p>
+      <p>{post.excerpt}</p>
+    </div>
+  {/each}
 </div>
 
 <style lang="scss">
-	.date {
-		font-size: var(--sl-font-size-small);
-	}
+  .date {
+    font-size: var(--sl-font-size-small);
+    display: flex;
+    flex-direction: row;
+    gap: 0.5em;
+    align-items: center;
+  }
 </style>
