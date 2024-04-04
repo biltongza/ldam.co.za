1a9096ab4d481aab29332d4a387a915b967c3aba
Logan Dam <dam.loganza@gmail.com>
2024-04-05 00:49:20 +0200

    remove angular site

--- frontend-ng/README.md
+++
@@ -1,6 +0,0 @@
-TODO: 
-- [x] Port application shell
-- [x] Fix social cards (meta implemented, doesn't work without SSR)
-- [x] Fix title
-- [ ] Fix image route not working on reload/direct access
-- [ ] Add blog 
--- frontend-ng/src/app/getRouteParam.function.ts
+++
@@ -1,9 +0,0 @@
-import { ActivatedRouteSnapshot } from '@angular/router';
-
-export function getRouteData(snapshot: ActivatedRouteSnapshot, param: string) {
-  let p = snapshot.data[param];
-  if (!p && snapshot.parent) {
-    p = getRouteData(snapshot.parent, param);
-  }
-  return p;
-}
