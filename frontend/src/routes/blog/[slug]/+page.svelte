a291e18749fca2ab68ee1e5ff0c40b148237ca2f
Logan Dam <dam.loganza@gmail.com>
2024-04-10 21:56:55 +0200

    trying out h-entry stuff

--- frontend/src/routes/blog/[slug]/+page.svelte
+++ frontend/src/routes/blog/[slug]/+page.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
-	import { metaStore, titleStore } from '$lib/stores';
-	import { DateFormat } from '$lib/__consts';
-	import dayjs from 'dayjs';
-	import { onDestroy } from 'svelte';
-	import type { PageData } from './$types';
-	export let data: PageData;
-	export let { post } = data;
+  import { DateFormat } from '$lib/__consts';
+  import { metaStore, titleStore } from '$lib/stores';
+  import dayjs from 'dayjs';
+  import { onDestroy } from 'svelte';
+  import type { PageData } from './$types';
+  export let data: PageData;
+  export let { post } = data;
 
-	titleStore.set(post.metadata.title);
-	metaStore.set({
-		'og:type': 'article',
-		'og:title': post.metadata.title,
-		'og:description': post.metadata.excerpt
-	});
+  titleStore.set(post.metadata.title);
+  metaStore.set({
+    'og:type': 'article',
+    'og:title': post.metadata.title,
+    'og:description': post.metadata.excerpt
+  });
 
-	onDestroy(() => {
-		titleStore.set(undefined);
-		metaStore.set(undefined);
-	});
+  onDestroy(() => {
+    titleStore.set(undefined);
+    metaStore.set(undefined);
+  });
 </script>
 
-<article>
-	<h2>{post.metadata.title}</h2>
-	<p class="date">{dayjs(post.metadata.date).format(DateFormat)}</p>
-	<p>
-		{#each post.metadata.tags as tag}
-			<sl-badge variant="primary" pill>{tag}</sl-badge>
-		{/each}
-	</p>
-	{@html post.content}
+<article class="h-entry">
+  <h2 class="p-name">{post.metadata.title}</h2>
+  <p class="date dt-published">{dayjs(post.metadata.date).format(DateFormat)}</p>
+  <p>
+    {#each post.metadata.tags as tag}
+      <sl-badge variant="primary" class="p-category" pill>{tag}</sl-badge>
+    {/each}
+  </p>
+  <div class="e-content">
+    {@html post.content}
+  </div>
 </article>
 
 <style lang="scss">
-	.date {
-		font-size: var(--sl-font-size-small);
-	}
+  .date {
+    font-size: var(--sl-font-size-small);
+  }
 </style>
