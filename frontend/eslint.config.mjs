94d769c445254b8ac96539477601f1ced0530756
Logan Dam <dam.loganza@gmail.com>
2024-11-19 22:30:12 +0100

    update eslint config

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,6 +3,7 @@
 import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginSvelte from 'eslint-plugin-svelte';
+import globals from 'globals';
 import svelteParser from 'svelte-eslint-parser';
 import ts from 'typescript-eslint';
 
@@ -16,17 +17,13 @@ export default ts.config(
   ...eslintPluginSvelte.configs['flat/prettier'],
   eslintConfigPrettier,
   {
-    files: ['**/*.svelte', '**/*.ts'],
+    files: ['*.{svelte,svelte.ts}', '**/*.{svelte,svelte.ts}'],
     languageOptions: {
       globals: {
         __VERSION__: 'readonly',
-        __LASTMOD__: 'readonly'
-      }
-    }
-  },
-  {
-    files: ['*.svelte', '**/*.svelte'],
-    languageOptions: {
+        __LASTMOD__: 'readonly',
+        ...globals.browser
+      },
       parser: svelteParser,
       parserOptions: {
         parser: ts.parser
